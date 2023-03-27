d04bea25e8ceb39990efe1a4a068331604efa493
JoseJorgeGC <jjgutierrez910@gmail.com>
2023-03-26 23:42:18 -0400

    Update settings to mysql DB.

--- system_junk/settings.py
+++ system_junk/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'system_junk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'junker',
         'USER': 'root',
         'PASSWORD': '',
