9e831a352b29a55d27157171f211a04e3b49291a
SoftimusPro <darielanielys@gmail.com>
2023-08-03 18:23:21 -0400

    menu desplegable para partes y modelos

--- inventary/admin.py
+++ inventary/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Cars, Brands, Models, Customer
+from .models import Cars, Brands, Models, Customer,PartType
 
 # Register your models here.
 admin.site.register(Cars)
 admin.site.register(Brands)
 admin.site.register(Models)
 admin.site.register(Customer)
+admin.site.register(PartType)
